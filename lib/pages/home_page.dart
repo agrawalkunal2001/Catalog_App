e91cfbda2f692180867a65ca4797fc8b52f62777
Plane <plane2001@outlook.com>
2021-11-02 19:41:35 +0530

    Home page layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
-
 import 'package:catalog/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:catalog/models/catalog.dart';
 import 'package:catalog/widgets/item.dart';
 import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -32,29 +32,81 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog"),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.logout),
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "Catalog App".text.xl5.bold.make(),
+              "Best Products".text.xl2.make(),
+              if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                CatalogList().expand()
+              else
+                Center(child: CircularProgressIndicator()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];
+        return CatalogItem(catalog: catalog);
+      },
+      itemCount: CatalogModel.items.length,
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+  const CatalogItem({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+      child: Row(
+        children: [
+          Image.network(catalog.image).box.p8.make().p16().w40(context),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                catalog.name.text.xl2.bold.make(),
+                catalog.desc.text.make(),
+                10.heightBox,
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  buttonPadding: EdgeInsets.zero,
+                  children: [
+                    "\$${catalog.price}".text.xl.bold.make(),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(StadiumBorder()),
+                      ),
+                      child: "Buy".text.make(),
+                    ),
+                  ],
+                ).pOnly(right: (12.0)),
+              ],
+            ),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemBuilder: (context, index) {
-                  return MyItem(item: CatalogModel.items[index]);
-                },
-                itemCount: CatalogModel.items.length,
-              )
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
-      ), // It is a kind of recycler view where the data which is not visible currently, renders and becomes visible when it is scrolled.
-      drawer: MyDrawer(),
-    );
+    ).white.roundedSM.square(150).make().py16();
   }
 }
