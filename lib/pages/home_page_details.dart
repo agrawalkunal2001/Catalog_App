1ccc5ed64778622597200b318e88ed104278d43a
Plane <plane2001@outlook.com>
2021-11-02 22:56:33 +0530

    Added item detail page

---
+++ lib/pages/home_page_details.dart
@@ -0,0 +1,62 @@
+import 'package:catalog/models/catalog.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+  const HomeDetailPage({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.xl4.bold.make(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all(StadiumBorder()),
+              ),
+              child: "Buy".text.make(),
+            ).wh(100, 50),
+          ],
+        ).p16(),
+      ),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(
+                    tag: Key(catalog.id.toString()),
+                    child: Image.network(catalog.image))
+                .h32(context),
+            Expanded(
+              child: VxArc(
+                height: 30,
+                arcType: VxArcType.CONVEY,
+                edge: VxEdge.TOP,
+                child: Container(
+                  width: context.screenWidth,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      catalog.name.text.xl5.bold.make(),
+                      catalog.desc.text.xl.make(),
+                    ],
+                  ).py64(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,4 +2,5 @@ class MyRoutes {
   // Static because it takes memory only once and does not need to create object of this class
   static String loginRoute = "/login";
   static String homeRoute = "/home";
+  static String homeDetailRoute = "/detail";
 }
